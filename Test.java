bb575da3b579b50d8b233b2cfa036d97c6a5b3bd
DingSJ <15225686971@163.com>
2016-03-19 21:59:59 +0800

    testDemo
    测试类

---
+++ Test.java
@@ -0,0 +1,11 @@
+/**
+	测试github的本地上传功能
+*/
+
+public class Test{
+	public static void main(String[] args) {
+		System.out.println("============================");
+		System.out.println("Hello! GitHub");
+		System.out.println("============================");
+	}
+}
